08c04f5bc214e1cedbc6d1e04af9a1a2d8eb019f
Jahirul83 <jahirulislamcse83.com>
2024-01-19 03:34:47 +0600

    parallax in cover

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
-
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from '../../../assets/menu/menu-bg.jpg'
 
 const Menu = () => {
     return (
@@ -7,6 +8,7 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Boss | menu</title>
             </Helmet>
+            <Cover img={menuImg} title="our menu"></Cover>
             <h1>menu</h1>
         </div>
     );
