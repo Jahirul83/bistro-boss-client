749bf4016f3c51097b76883a2d1742821de2c5a1
Jahirul83 <jahirulislamcse83@gmail.com>
2024-03-09 00:57:34 +0600

    tabs card shown

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -4,14 +4,16 @@ import orderCoverImg from '../../../assets/shop/banner2.jpg'
 import Cover from '../../Shared/Cover/Cover';
 import { useState } from 'react';
 import useMenu from '../../../hooks/useMenu';
+// import FoodCard from '../../../Components/FoodCard/FoodCard';
+import OrderTab from '../OrderTab/OrderTab';
 const Order = () => {
-    const [tabIndex,setTabIndex] = useState(0);
+    const [tabIndex, setTabIndex] = useState(0);
     const [menu] = useMenu();
     const dessert = menu.filter(item => item.category === "dessert");
     const soup = menu.filter(item => item.category === "soup");
     const salad = menu.filter(item => item.category === "salad");
     const pizza = menu.filter(item => item.category === "pizza");
-    const offered = menu.filter(item => item.category === "offered");
+    const drinks = menu.filter(item => item.category === "drinks");
     return (
         <div>
             <Cover img={orderCoverImg} title="Order Food"></Cover>
@@ -23,11 +25,21 @@ const Order = () => {
                     <Tab>Dessert</Tab>
                     <Tab>Drinks</Tab>
                 </TabList>
-                <TabPanel></TabPanel>
-                <TabPanel></TabPanel>
-                <TabPanel></TabPanel>
-                <TabPanel></TabPanel>
-                <TabPanel></TabPanel>
+                <TabPanel>
+                    <OrderTab item={salad}></OrderTab>
+                </TabPanel>
+                <TabPanel>
+                    <OrderTab item={pizza}></OrderTab>
+                </TabPanel>
+                <TabPanel>
+                    <OrderTab item={soup}></OrderTab>
+                </TabPanel>
+                <TabPanel>
+                    <OrderTab item={dessert}></OrderTab>
+                </TabPanel>
+                <TabPanel>
+                    <OrderTab item={drinks}></OrderTab>
+                </TabPanel>
             </Tabs>
         </div>
     );
