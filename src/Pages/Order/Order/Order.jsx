d6e1e9ac109a285c049576c4ade2e60604b57411
Jahirul83 <jahirulislamcse83@gmail.com>
2024-03-08 23:59:47 +0600

    Order page and tabs implemented

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,9 +1,34 @@
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import orderCoverImg from '../../../assets/shop/banner2.jpg'
 import Cover from '../../Shared/Cover/Cover';
+import { useState } from 'react';
+import useMenu from '../../../hooks/useMenu';
 const Order = () => {
+    const [tabIndex,setTabIndex] = useState(0);
+    const [menu] = useMenu();
+    const dessert = menu.filter(item => item.category === "dessert");
+    const soup = menu.filter(item => item.category === "soup");
+    const salad = menu.filter(item => item.category === "salad");
+    const pizza = menu.filter(item => item.category === "pizza");
+    const offered = menu.filter(item => item.category === "offered");
     return (
         <div>
             <Cover img={orderCoverImg} title="Order Food"></Cover>
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList>
+                    <Tab>Salad</Tab>
+                    <Tab>pizza</Tab>
+                    <Tab>Soup</Tab>
+                    <Tab>Dessert</Tab>
+                    <Tab>Drinks</Tab>
+                </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
         </div>
     );
 };
