867faf3a9b0e522e3444d9f0f2ada111485f4781
Jahirul83 <jahirulislamcse83.com>
2024-03-08 23:59:47 +0600

    Order page and tabs implemented

--- src/Pages/Shared/MenuItem/MenuItem.jsx
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -1,10 +1,10 @@
 
 
-const MenuItem = ({item}) => {
-    const {image,name,recipe,price} =item
+const MenuItem = ({ item }) => {
+    const { image, name, recipe, price } = item;
     return (
         <div className="flex space-x-2">
-            <img style={{borderRadius: '0px 200px 200px 200px'}} className="w-[100px]" src={image} alt="" />
+            <img style={{ borderRadius: '0px 200px 200px 200px' }} className="w-[100px]" src={image} alt="" />
             <div>
                 <h3 className="uppercase">{name}----------</h3>
                 <p>{recipe}</p>
