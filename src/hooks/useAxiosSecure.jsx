cf99bd39c5b7887e5f8585c9392c72dee394f458
Jahirul83 <jahirulislamcse83@gmail.com>
2024-06-13 21:11:48 +0600

    for 401 or 403 logOut the user and move the navigate to the login

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
 
+    const navigate = useNavigate();
+    const { logOut } = useAuth();
     /* request interceptor to add authorization header for every secure call to the api */
 
     axiosSecure.interceptors.request.use(function (config) {
@@ -20,9 +24,16 @@ const useAxiosSecure = () => {
     // interceptors 401 and 403 status
     axiosSecure.interceptors.response.use(function (response) {
         return response
-    }, (error) => {
+    },async (error) => {
         const status = error.response.status;
-        console.log('status error in the interceptors',status);
+        console.log('status error in the interceptors', status);
+
+        // for 401 or 403 logOut the user and move the navigate to the login
+
+        if (status === 401 || status === 403) {
+            await logOut();
+            navigate('/login');
+        }
         return Promise.reject(error);
     })
 
