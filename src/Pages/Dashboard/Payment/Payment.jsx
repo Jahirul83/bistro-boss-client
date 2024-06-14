fad23015aeff1f3defbfaaa9f8bd6fb6d5802827
Jahirul83 <jahirulislamcse83@gmail.com>
2024-06-14 21:36:46 +0600

    create payment route

---
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,15 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+
+
+const Payment = () => {
+    return (
+        <div>
+            <SectionTitle heading="Payment" subHeading="please pay to eat"></SectionTitle>
+            <div>
+                <h2 className="text-4xl">taka tumi uira uira aso</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Payment;
