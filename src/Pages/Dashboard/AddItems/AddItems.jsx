95cb4a943d0f5d2848f3a43d86bd298c00dacb5e
Jahirul83 <jahirulislamcse83@gmail.com>
2024-06-14 16:35:27 +0600

    item inserted in database

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa6";
 import useAxiosPublic from '../../../hooks/useAxiosPublic';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
@@ -10,6 +11,7 @@ const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_ke
 const AddItems = () => {
     const { register, handleSubmit } = useForm();
     const axiosPublic = useAxiosPublic();
+    const axiosSecure = useAxiosSecure();
 
 
     const onSubmit = async (data) => {
@@ -22,7 +24,24 @@ const AddItems = () => {
                     'Content-Type': 'multipart/form-data'
                 }
             });
-            console.log(res.data);
+            // console.log(res.data);
+            if(res.data.success){
+                // now send the menu item data in the server with image
+                const menuItem = {
+                    name: data.name,
+                    category: data.category,
+                    price: parseFloat(data.price),
+                    recipe: data.recipe,
+                    image: res.data.data.display_url
+                    
+                }
+                const menuRes = await axiosSecure.post('/menu',menuItem);
+                console.log(menuRes.data);
+
+                if(menuRes.data.insertedId){
+                    // popup
+                }
+            }
     };
     return (
         <div>
