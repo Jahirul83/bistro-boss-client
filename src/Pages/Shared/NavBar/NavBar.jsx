a55066eabbfa7fcec3440f269f383b102bb4b4c3
Jahirul83 <jahirulislamcse83@gmail.com>
2024-06-15 13:52:15 +0600

    differnt dashboard for user and admin

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 import { FaCartShopping } from "react-icons/fa6";
 import useCart from "../../../hooks/useCart";
+import useAdmin from "../../../hooks/useAdmin";
 
 
 
 const NavBar = () => {
     const { user, logOut } = useContext(AuthContext);
+    const [isAdmin] = useAdmin();
     const [cart] = useCart();
 
     const handleLogOut = () => {
@@ -20,7 +22,19 @@ const NavBar = () => {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/menu">Our Menu</Link></li>
         <li><Link to="/Order/salad">Order Food</Link></li>
-        <li><Link to="/secret">Secret</Link></li>
+        {
+            // user ? 'true' : 'false'
+            // user ? condition ? 'double true': 'one true' : 'false'
+        }
+        {
+            user && isAdmin && <li><Link to="/dashboard/adminHome">Dashboard</Link></li>
+        }
+        {
+            user && !isAdmin && <li><Link to="/dashboard/userHome">Dashboard</Link></li>
+        }
+
+
+
         <li>
             <Link to="/dashboard/cart">
                 <button className="btn">
